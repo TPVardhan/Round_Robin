1a655365037e83866136f042583bf14d37dbe32b
TPVardhan <49223573+TPVardhan@users.noreply.github.com>
2019-04-24 20:59:27 +0530

    Update RR.c

--- RR.c
+++ RR.c
@@ -10,7 +10,7 @@ int main()
   remain=n; 
   for(count=0;count<n;count++) 
   { 
-    printf("\n\tEnter Arrival Time and Burst Time for Process Process Number %d :",count+1); 
+    printf("Enter Arrival Time and Burst Time for Process Process Number %d :",count+1); 
     scanf("%d",&at[count]); 
     scanf("%d",&bt[count]); 
     rt[count]=bt[count]; 
@@ -46,8 +46,8 @@ int main()
     else 
       count=0; 
   } 
-  printf("\n\tAverage Waiting Time= %f\n",wait_time*1.0/n); 
-  printf("\n\tAvg Turnaround Time = %f",turnaround_time*1.0/n); 
+  printf("Average Waiting Time= %f\n",wait_time*1.0/n); 
+  printf("Avg Turnaround Time = %f",turnaround_time*1.0/n); 
   
   return 0; 
 }
